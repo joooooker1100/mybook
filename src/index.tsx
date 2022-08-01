9726bab201705a0f9442259a97ffb01002f9adec
nadernmds <nadernmds@gmail.com>
2022-08-01 15:05:07 +0430

    ...

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Users from "./Users";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <Users />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="users" element={<Users />} />
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
