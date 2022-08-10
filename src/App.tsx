4dac7b896ce3cfb4f444556205d31caf72af1cc0
nadernmds <nadernmds@gmail.com>
2022-08-10 20:14:06 +0430

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import twelvedata from "twelvedata";
-export default function App(obj: any) {
+
+export default function App() {
+  const [ethData, setEthData] = useState([]);
   const config = {
     key: "47d4013b04a5446da43b62888daf57c3",
   };
-  const client = twelvedata(config);
-  client
-    .timeSeries({ symbol: "ETH/BTC", interval: "1min", outputsize: 5 })
-    .then((data) => {})
-    .catch((error) => {
-      // handle error
-    });
+  useEffect(() => {
+    const client = twelvedata(config);
+    client
+      .timeSeries({ symbol: "ETH/BTC", interval: "1min", outputsize: 5 })
+      .then((data) => {
+        setEthData(data.values);
+      })
+      .catch((error) => {
+        // handle error
+      });
+  }, []);
 
-  let data = [];
-  for (const key in obj) {
-    const element = obj[key];
-    data.push(element);
-  }
-  return data.map((x, index) => ({
-    index,
-    open: +x.open,
-    close: +x.close,
-    high: +x.high,
-    low: +x.low,
-    color: +x.close - +x.open > 0 ? "green" : "red",
-    time: x.datetime,
-    ts: (new Date(x.datetime).getTime() / 1000).toFixed(),
-  }));
+  return (
+    <div>
+      {ethData.map((w: any) => {
+        return w.open;
+      })}
+    </div>
+  );
 }
