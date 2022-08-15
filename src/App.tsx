ae6fc36f95cf69f2643ac61af57ee138cddfbfb5
nadernmds <nadernmds@gmail.com>
2022-08-15 19:33:12 +0430

    ....

--- src/App.tsx
+++ src/App.tsx
@@ -4,37 +4,30 @@ import twelvedata from "twelvedata";
 export default function App() {
   const [ethData, setEthData] = useState([]);
   const config = {
-    key: "47d4013b04a5446da43b62888daf57c3",
+    key: "3e9efaf9297541cba9bc6da6d7351782",
   };
   useEffect(() => {
-    
-    const client = twelvedata(config);
-    client
-      .timeSeries({ symbol: "ETH/BTC", interval: "1min", outputsize: 1 })
-      .then((data) => {
-        setEthData(data.values);
-      })
-      .catch((error) => {
-        // handle error
-      });
-  }, []);
+    let a = setInterval(() => {
+      const client = twelvedata(config);
+      client
+        .timeSeries({ symbol: "BTC/USD", interval: "1min", outputsize: 1 })
+        .then((data) => {
+          console.log(data);
+          setEthData(data.values);
+        });
+    }, 60000);
+    return () => {
+      clearInterval(a);
+    };
+  });
 
   return (
     <div>
-      {Close()}
+      {ethData.map((w: any) => {
+        console.log(w.close);
+
+        return (+w.close).toLocaleString("en-US", { maximumFractionDigits: 5 });
+      })}
     </div>
   );
-  function Close() {
-    
-    useEffect(()=>{
-      setInterval(App,10000)
-    })
-    return(
-      <div>
-        {ethData.map((w:any)=>{
-          return(w.close)
-        })}
-      </div>
-    )
-  }
 }
