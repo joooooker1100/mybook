c5f8c9cab8778bbd52facae3fd64528f3a7c9523
nadernmds <nadernmds@gmail.com>
2022-08-16 20:33:27 +0430

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,20 @@
 import { useEffect, useState } from "react";
-import twelvedata from "twelvedata";
 
 export default function App() {
   const [ethData, setEthData] = useState([]);
-  const config = {
-    key: "3e9efaf9297541cba9bc6da6d7351782",
-  };
+
   useEffect(() => {
     let a = setInterval(() => {
-      const client = twelvedata(config);
-      client
-        .timeSeries({ symbol: "BTC/USD", interval: "1min", outputsize: 1 })
-        .then((data) => {
-          console.log(data);
-          setEthData(data.values);
+      fetch("https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT")
+        .then((w) => w.json())
+        .then((w) => {
+          setEthData(w.price);
         });
-    }, 60000);
+    }, 1000);
     return () => {
       clearInterval(a);
     };
-  });
+  }, []);
 
-  return (
-    <div>
-      {ethData.map((w: any) => {
-        return (+w.close).toLocaleString("en-US", { maximumFractionDigits: 5 });
-      })}
-    </div>
-  );
+  return <div>{ethData}</div>;
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,23 +4,19 @@ import "./index.css";
 import App from "./App";
 
 import { Home } from "@mui/icons-material";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import reportWebVitals from "./reportWebVitals";
-import Close from "./Close";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/"element={<App/>}/>
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
